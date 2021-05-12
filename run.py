4dc14191b42872446e42f0f3795f539dc5474e52
davepeyton <davepeytonmusic@gmail.com>
2021-05-12 12:38:14 +0000

    added member html

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", page_title="Home")
+    return render_template("index.html")
 
 
 @app.route("/about")
@@ -19,16 +19,15 @@ def about():
     return render_template("about.html", page_title="About", company=data)
 
 
-    @app.route("/about/<member_name>")
-    def about_member(member_name):
-        member = {}
-        with open("data/company.json", "r") as json_data:
-            data = json.load(json_data)
-            for obj in data:
-                if obj["url"] == member_name:
-                    member = obj
-        return "<h1>" + member["name"] + "</h1>"            
-         
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
@@ -45,5 +44,5 @@ def careers():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        port=int(os.environ.get("PORT", "8080")),
         debug=True)
